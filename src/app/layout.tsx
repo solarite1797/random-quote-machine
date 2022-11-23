b5070336fcc969da4a269231a899da80d5f4fe78
Luka <47296785+lukadev-0@users.noreply.github.com>
2022-11-23 20:39:09 +0100

    feat:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
       <body className="bg-gray-200 text-black dark:bg-gray-900 dark:text-white">
         <header className="bg-gray-200/40 dark:bg-gray-900/40 backdrop-blur fixed top-0 w-full">
           <div className="max-w-screen-lg mx-auto h-16 flex items-center px-4">
-            <Link href="/" className="flex space-x-3 items-center">
+            <div className="flex space-x-3 items-center">
               <Logo className="w-10 h-10" />
               <span>Random Quote Machine</span>
-            </Link>
+            </div>
           </div>
         </header>
 
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>random quote machine</h1>
-    </div>
-  )
-}
